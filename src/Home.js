7289d5018d163723eef776d7e4403fbbabe19700
Antonello Mangone <antonello.mangone@gmail.com>
2026-09-18 09:00:24 +0200

    sorgente studio react

---
+++ src/Home.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import BlogList from "./BlogList";
+
+const Home = () => {
+
+    const [blogs, setBlogs] = useState([
+        {id:1, title:"My new website", author:"mario", body:"lorem ipsum..."},
+        {id:2, title:"Welcome party!", author:"yoshi", body:"lorem ipsum..."},
+        {id:3, title:"Web dev top tips", author:"mario", body:"lorem ipsum..."}
+    ]);
+
+    return (
+        <div className="home">
+            <BlogList blogs={blogs} title="All Blogs!" />
+            <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's Blogs!" />
+        </div>
+    );
+}
+ 
+export default Home;
