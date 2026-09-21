d5664a90444da3142b1ac8c6fa49a831a6ae2738
Antonello Mangone <antonello.mangone@gmail.com>
2026-09-21 16:37:59 +0200

    learning react

--- src/Home.js
+++ src/Home.js
@@ -9,10 +9,15 @@ const Home = () => {
         {id:3, title:"Web dev top tips", author:"mario", body:"lorem ipsum..."}
     ]);
 
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    }
+
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs!" />
-            <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's Blogs!" />
+            <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
+            {/*<BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's Blogs!" />*/}
         </div>
     );
 }
