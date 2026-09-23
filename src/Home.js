2479aac7906a132f14ae63c7b012e35250060a22
Antonello Mangone <antonello.mangone@gmail.com>
2026-09-23 16:22:14 +0200

    implementazione app React

--- src/Home.js
+++ src/Home.js
@@ -1,23 +1,36 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
 
-    const [blogs, setBlogs] = useState([
-        {id:1, title:"My new website", author:"mario", body:"lorem ipsum..."},
-        {id:2, title:"Welcome party!", author:"yoshi", body:"lorem ipsum..."},
-        {id:3, title:"Web dev top tips", author:"mario", body:"lorem ipsum..."}
-    ]);
+    // const [blogs, setBlogs] = useState([
+    //     {id:1, title:"My new website", author:"mario", body:"lorem ipsum..."},
+    //     {id:2, title:"Welcome party!", author:"yoshi", body:"lorem ipsum..."},
+    //     {id:3, title:"Web dev top tips", author:"mario", body:"lorem ipsum..."}
+    // ]);
+
+    const [blogs, setBlogs] = useState(null);    
+
+    const [name, setName] = useState('mario');
 
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
     }
 
+    useEffect(() => {
+        fetch('http://localhost:8000/blogs')
+        .then((res) => {
+            return res.json();
+        })
+        .then((data) => {
+            setBlogs(data);
+        })
+    }, []);
+
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
-            {/*<BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's Blogs!" />*/}
+            { blogs && <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} /> }
         </div>
     );
 }
